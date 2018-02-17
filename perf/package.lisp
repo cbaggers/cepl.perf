50ebeccc42f6f244d12b34bcc6d23f3666fd61a8
cbaggers <chris.bagley@gmail.com>
2018-02-17 13:39:19 +0100

    CEPL no longer depends on (or reexports) rtg-math

--- perf/package.lisp
+++ perf/package.lisp
@@ -1,7 +1,7 @@
 ;;;; package.lisp
 
 (uiop:define-package #:cepl.perf
-    (:use #:cl #:cepl.build #:%rtg-math)
+    (:use #:cl #:cepl.build #:cepl.defn)
   (:export :profile
            :load-with-instrumentation
            :start-profiling
